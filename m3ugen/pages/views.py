e68fe11a6e997cfd2864afbb1082e09aaaf591ad
ValerySkibitski <81686555+ValerySkibitski@users.noreply.github.com>
2021-06-27 17:45:55 +0300

    add update page

--- m3ugen/pages/views.py
+++ m3ugen/pages/views.py
@@ -6,10 +6,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
-    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
-    # context = [
-
-    # ]
     return render(request, 'home.html') # HttpResponse("<h1>Hello World</h1>") # string of HTML code
 
 def uploadM3U(request):
@@ -19,30 +15,32 @@ def uploadM3U(request):
     }
     return render(request, 'upload.html', context)
 
+def updateM3U(request, id):
+    server = listservers.objects.get(id=id)
+    url = server.ipNameServer
+    r = requests.get(url)
+
+    context = {
+        'list1': server,
+        'url': url,
+        'm3u': r.text,
+    }
+    return render(request, 'update.html', context)
+
 
 def listM3U(request):
     servers = listservers.objects.all()
-    context = {
-        'list2': servers,
-    }
-    url = servers[0].ipNameServer
-        
-    # fp = urllib.request.urlopen("http://www.python.org")
-    # mybytes = fp.read()
-
-    # mystr = mybytes.decode("utf8")
-    # fp.close()
-    # print(mystr)
+    url = servers[1].ipNameServer
     
     print ('url:' + url)
-    http=''
-    # if 'http' in url:
-    #     http = urllib.request.urlopen(url)
-    #     mybytes = http.read()
-    #     mystr = mybytes.decode("utf8")
-    #     http.close()
     r = requests.get(url)
+    #servers[0].contentm3u = r.text
     print(r.text)
+
+    context = {
+        'list2': servers,
+        'm3u': r.text,
+    }
 
     return render(request, 'm3uList.html', context)
  
