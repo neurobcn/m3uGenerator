59e4a651d227c91cce92e2add559b7844a65c550
ValerySkibitski <81686555+ValerySkibitski@users.noreply.github.com>
2021-06-30 18:49:33 +0300

    Update settings.py

--- m3ugen/m3ugen/settings.py
+++ m3ugen/m3ugen/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,11 +125,14 @@ USE_TZ = True
 #STATIC_URL = '/static/'
 
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.joinpath('static/'),]
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+
+
 
 
 # Default primary key field type
