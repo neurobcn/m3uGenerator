0fe3777057c890d6c7fb4ee59851b8d46890ea38
ValerySkibitski <81686555+ValerySkibitski@users.noreply.github.com>
2021-07-06 11:25:42 +0300

    delete django_tables2

--- m3ugen/m3ugen/settings.py
+++ m3ugen/m3ugen/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_tables2',
+    #'django_tables2',
     'm3uservers',
     #'pages',
 ]
--- m3ugen/m3uservers/tables.py
+++
@@ -1,8 +0,0 @@
-import django_tables2 as tables
-from .models import Canal
-
-class PersonTable(tables.Table):
-    class Meta:
-        model = Canal
-        template_name = "django_tables2/bootstrap.html"
-        fields = ("idCanal", "nameCanal", "nameGroup", "checkedForOutput")
